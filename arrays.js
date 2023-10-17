5b20c14fc74eeb0de0d9e6a67c5c3f4b4fb282d2
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-10-17 17:28:49 -0400

    Add coding exercises

---
+++ arrays.js
@@ -0,0 +1,48 @@
+let fruit = document.querySelector(".fruit");
+let flower = document.querySelector(".flower");
+let color = document.querySelector(".color");
+let age = document.querySelector(".age");
+
+
+// CODE ALONG
+// 1. Update the variable fruitList so that it is an array with at least 4 fruits.
+let fruitList = [];
+console.log();
+
+// 2. Update the variable flowerList so that it is an array with at least 4 flowers.
+let flowerList = [];
+console.log();
+
+// 3. Update the variable colorList so that it is an array with at least 4 different colors.
+let colorList = [];
+console.log();
+
+// 4. Update the variable ageList so that it is an array with a list of ages (numbers) of classmates and volunteers.
+let ageList = [];
+console.log();
+
+
+// CODE SOLO
+// 5. Update the variable favFruit to retrieve the array element of your favorite fruit in the fruitList array. 
+// 6. Update the innerHTML to the favFruit variable.
+let favFruit = '';
+fruit.innerHTML = '';
+
+
+// 7. Update the variable favFlower to retrieve the array element of your favorite flower in the flowerList array. 
+// 8. Update the innerHTML to the favFlower variable.
+let favFlower = '';
+flower.innerHTML = '';
+
+// 9. Update the variable favColor to retrieve your favorite color from the colorList array. 
+// 10.Update the innerHTML to the favColor variable.
+let favColor = '';
+color.innerHTML = '';
+
+
+// 11. Update the variable oldestAge to retrieve the array element of the oldest person in the ageList array. 
+// 12. Update the innerHTML to the oldestAge variable.
+let oldestAge = '';
+age.innerHTML = '';
+
+
---
+++ script.js
@@ -0,0 +1,24 @@
+// 1. Covert to an arrow function.
+function greet() {
+    return "Hello, world!";
+  }
+  
+
+// 2. Convert to an arrow function.
+function double(number) {
+    return number * 2;
+  }
+  
+
+// 3. Convert to an arrow function.
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  
+
+console.log(sayHello())  // should print "Hello, world!"
+console.log(double(3)) // should print 6
+console.log(multiply(3, 7)) // should print 21
+  
+  
